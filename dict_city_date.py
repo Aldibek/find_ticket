07719a9e562f647e426d69388462a39966df5b72
Aldibek <aldibek.nurdaulet@gmail.com>
2026-05-23 01:36:36 +0500

    Ну бета версия в целом работает но надо доработать

---
+++ dict_city_date.py
@@ -0,0 +1,307 @@
+CITY = {
+    # Алматы
+    "Алматы": "alma-ata",
+    "Almaty": "alma-ata",
+    "Alma-Ata": "alma-ata",
+
+    "Алматы 1": "almaty-1",
+    "Алматы-1": "almaty-1",
+    "Almaty 1": "almaty-1",
+    "Almaty-1": "almaty-1",
+
+    "Алматы 2": "almaty-2",
+    "Алматы-2": "almaty-2",
+    "Almaty 2": "almaty-2",
+    "Almaty-2": "almaty-2",
+
+    # Иногда в старых ссылках Freedom встречается так
+    "Алма-Ата 2": "alma-ata-2",
+    "Alma-Ata 2": "alma-ata-2",
+
+    # Астана
+    "Астана": "astana",
+    "Astana": "astana",
+
+    "Нур-Султан": "nur-sultan",
+    "Nur-Sultan": "nur-sultan",
+
+    "Астана 1": "astana-1",
+    "Астана-1": "astana-1",
+    "Astana 1": "astana-1",
+    "Astana-1": "astana-1",
+
+    "Нурлы Жол Астана": "nurly-jol-astana",
+    "Нурлы-Жол Астана": "nurly-jol-astana",
+    "Nurly Zhol Astana": "nurly-jol-astana",
+    "Nurly-Jol Astana": "nurly-jol-astana",
+
+    "Астана Нурлы Жол": "astana-nurly-jol",
+    "Астана-Нурлы Жол": "astana-nurly-jol",
+    "Astana Nurly Zhol": "astana-nurly-jol",
+
+    # Большие города / станции
+    "Шымкент": "shymkent",
+    "Shymkent": "shymkent",
+
+    "Актобе": "aktobe",
+    "Aktobe": "aktobe",
+
+    "Атырау": "atyrau",
+    "Atyrau": "atyrau",
+
+    "Актау": "aktau",
+    "Aktau": "aktau",
+
+    "Уральск": "uralsk",
+    "Uralsk": "uralsk",
+
+    "Тараз": "taraz",
+    "Taraz": "taraz",
+
+    "Туркестан": "turkestan",
+    "Turkestan": "turkestan",
+
+    "Кызылорда": "kyzylorda",
+    "Kyzylorda": "kyzylorda",
+
+    "Павлодар": "pavlodar",
+    "Pavlodar": "pavlodar",
+
+    "Петропавловск": "petropavlovsk",
+    "Petropavlovsk": "petropavlovsk",
+
+    "Костанай": "kostanai",
+    "Kostanay": "kostanai",
+
+    "Кустанай": "kustanai",
+    "Kustanai": "kustanai",
+
+    "Кокшетау 1": "kokshetau-1",
+    "Кокшетау-1": "kokshetau-1",
+    "Kokshetau 1": "kokshetau-1",
+
+    # Караганда
+    "Караганды Пасс": "karagandy-pass",
+    "Караганды-Пасс": "karagandy-pass",
+    "Karagandy Pass": "karagandy-pass",
+
+    "Караганды Сорт": "karagandy-sort",
+    "Караганды-Сорт": "karagandy-sort",
+    "Karagandy Sort": "karagandy-sort",
+
+    # Другие найденные станции
+    "Алга": "alga",
+    "Alga": "alga",
+
+    "Алтынколь": "altynkol",
+    "Altynkol": "altynkol",
+
+    "Аральское Море": "aralskoe-more",
+    "Aralskoe More": "aralskoe-more",
+
+    "Арыс 2": "arys-2",
+    "Арыс-2": "arys-2",
+    "Arys 2": "arys-2",
+
+    "Атбасар": "atbasar",
+    "Atbasar": "atbasar",
+
+    "Балаш 1": "balhash-1",
+    "Балхаш 1": "balhash-1",
+    "Балхаш-1": "balhash-1",
+    "Balhash 1": "balhash-1",
+
+    "Балхаш 2": "balhash-2",
+    "Балхаш-2": "balhash-2",
+    "Balhash 2": "balhash-2",
+
+    "Бейнеу": "beineu",
+    "Beineu": "beineu",
+
+    "Бурное": "burnoe",
+    "Burnoe": "burnoe",
+
+    "Державинская": "derjavinskaya",
+    "Derjavinskaya": "derjavinskaya",
+
+    "Джусалы": "djusaly",
+    "Djusaly": "djusaly",
+
+    "Достык": "dostyk",
+    "Dostyk": "dostyk",
+
+    "Доссор": "dossor",
+    "Dossor": "dossor",
+
+    "Ерейментау": "ereimentau",
+    "Ereimentau": "ereimentau",
+
+    "Есиль": "esil",
+    "Esil": "esil",
+
+    "Жангиз-Тобе": "jangiz-tobe",
+    "Жангиз Тобе": "jangiz-tobe",
+    "Jangiz-Tobe": "jangiz-tobe",
+
+    "Жезказган": "jezkazgan",
+    "Jezkazgan": "jezkazgan",
+
+    "Железорудная": "jelezorudnaya",
+    "Jelezorudnaya": "jelezorudnaya",
+
+    "Жем": "jem",
+    "Jem": "jem",
+
+    "Жанакорган": "janakorgan",
+    "Janakorgan": "janakorgan",
+
+    "Защита": "zaschita",
+    "Zaschita": "zaschita",
+
+    "Илецк 1": "iletsk-1",
+    "Илецк-1": "iletsk-1",
+    "Iletsk 1": "iletsk-1",
+
+    "Казахстан": "kazahstan",
+    "Kazahstan": "kazahstan",
+
+    "Казалы": "kazaly",
+    "Kazaly": "kazaly",
+
+    "Кандыагаш": "kandyagash",
+    "Kandyagash": "kandyagash",
+
+    "Капчагай": "kapchagai",
+    "Kapchagai": "kapchagai",
+
+    "Коктума": "koktuma",
+    "Koktuma": "koktuma",
+
+    "Кульсары": "kulsary",
+    "Kulsary": "kulsary",
+
+    "Мангистау": "mangistau",
+    "Mangistau": "mangistau",
+
+    "Мангышлак": "mangyshlak",
+    "Mangyshlak": "mangyshlak",
+
+    "Манкент": "mankent",
+    "Mankent": "mankent",
+
+    "Опорная": "opornaya",
+    "Opornaya": "opornaya",
+
+    "Отар": "otar",
+    "Otar": "otar",
+
+    "Риддер": "ridder",
+    "Ridder": "ridder",
+
+    "Саксаульская": "saksaulskaya",
+    "Saksaulskaya": "saksaulskaya",
+
+    "Сары-Агач": "sary-agach",
+    "Сары Агач": "sary-agach",
+    "Sary-Agach": "sary-agach",
+
+    "Сары-Шаган": "sary-shagan",
+    "Сары Шаган": "sary-shagan",
+    "Sary-Shagan": "sary-shagan",
+
+    "Сексеул": "sekseul",
+    "Sekseul": "sekseul",
+
+    "Тобол": "tobol",
+    "Tobol": "tobol",
+
+    "Торетам": "toretam",
+    "Toretam": "toretam",
+
+    "Тюратам": "tyuratam",
+    "Tyuratam": "tyuratam",
+
+    "Тюлькубас": "tyulkubas",
+    "Tyulkubas": "tyulkubas",
+
+    "Уш-Тобе": "ush-tobe",
+    "Уш Тобе": "ush-tobe",
+    "Ush-Tobe": "ush-tobe",
+
+    "Хромтау": "hromtau",
+    "Hromtau": "hromtau",
+
+    "Чиили": "chiili",
+    "Chiili": "chiili",
+
+    "Шалкар": "shalkar",
+    "Shalkar": "shalkar",
+
+    "Шиели": "shieli",
+    "Shieli": "shieli",
+
+    "Шу": "shu",
+    "Shu": "shu",
+
+    "Чу": "chu",
+    "Chu": "chu",
+}
+import re
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+
+
+MONTHS_RU = {
+    "январь": "январь",
+    "января": "январь",
+
+    "февраль": "февраль",
+    "февраля": "февраль",
+
+    "март": "март",
+    "марта": "март",
+
+    "апрель": "апрель",
+    "апреля": "апрель",
+
+    "май": "май",
+    "мая": "май",
+
+    "июнь": "июнь",
+    "июня": "июнь",
+
+    "июль": "июль",
+    "июля": "июль",
+
+    "август": "август",
+    "августа": "август",
+
+    "сентябрь": "сентябрь",
+    "сентября": "сентябрь",
+
+    "октябрь": "октябрь",
+    "октября": "октябрь",
+
+    "ноябрь": "ноябрь",
+    "ноября": "ноябрь",
+
+    "декабрь": "декабрь",
+    "декабря": "декабрь",
+}
+
+
+
+
+MONTH_NUM = {
+    "январь": 1, "января": 1,
+    "февраль": 2, "февраля": 2,
+    "март": 3, "марта": 3,
+    "апрель": 4, "апреля": 4,
+    "май": 5, "мая": 5,
+    "июнь": 6, "июня": 6,
+    "июль": 7, "июля": 7,
+    "август": 8, "августа": 8,
+    "сентябрь": 9, "сентября": 9,
+    "октябрь": 10, "октября": 10,
+    "ноябрь": 11, "ноября": 11,
+    "декабрь": 12, "декабря": 12,
+}
--- parsing.py
+++ parsing.py
@@ -1,1 +1,175 @@
-print(1)
+
+ 
+from langchain_core.tools import tool
+import agentql
+import json
+import os
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+from time import sleep
+from dict_city_date import CITY,MONTH_NUM,MONTHS_RU
+from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+
+load_dotenv()
+
+From = ""
+To = ""
+openai_api_key = os.getenv("OPENAI_API_KEY")
+agenql_api_key = os.getenv("AGENTQL_API_KEY")
+
+
+while True:
+    try:
+        From = CITY[input('Откуда: ')]
+        To = CITY[input('Куда: ')]
+        break
+    except KeyError:
+        print("Город не найден. Пожалуйста, проверьте правильность ввода.")
+        
+print("Введите интервал времени где вы хотите поехать:(Например 23.02) ")
+date_from = input('От: ')
+date_to = input('До: ')
+url = f'https://freedom-travel.kz/railways/direction/{From}_{To}/'
+
+
+
+
+from datetime import datetime, timedelta
+
+
+def parse_input_date(text, year=2026):
+    return datetime.strptime(f"{text}.{year}", "%d.%m.%Y").date()
+
+
+def generate_dates(date_from, date_to, year=2026):
+    start = parse_input_date(date_from, year)
+    end = parse_input_date(date_to, year)
+
+    if end < start:
+        end = parse_input_date(date_to, year + 1)
+
+    current = start
+
+    while current <= end:
+        yield current
+        current += timedelta(days=1)
+
+
+def build_date_url(start_url, current_date):
+    date_for_url = current_date.strftime("%d.%m.%Y")
+    return start_url + date_for_url
+def get_ticket_links(date_from: str, date_to: str) -> list[str]:
+    """
+    Возвращает ссылки на страницы билетов за период дат.
+    """
+    return get_links(url, date_from, date_to)
+
+def get_links(url, date_from, date_to):
+    """
+    Получает ссылки на страницы с билетами для каждой даты в указанном диапазоне.
+    Использует Playwright для взаимодействия с сайтом и получения динамически загружаемых данных.
+    и возвращает список ссылок на страницы с билетами для каждой даты.Чтобы потом с ними работать и парсить информацию о билетах.
+    """
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(url)
+        timesleep = 5
+        page.wait_for_timeout(timesleep)
+        page.get_by_text("Найти билеты").click()
+        page.wait_for_timeout(5000)
+        new_url = page.url
+        #print("OLD URL:", old_url)
+
+    
+        new_url = page.url
+        print(new_url)
+        start_url = new_url[:-10]
+        link = []
+        for current_date in generate_dates(date_from, date_to):
+            url = build_date_url(start_url, current_date)
+            link.append(url)
+            #print(f"Проверяю дату {current_date.strftime('%d.%m.%Y')}")
+            #print(url)
+        browser.close()
+    return link
+    #page.goto(url, wait_until="domcontentloaded")
+    #page.wait_for_timeout(1000)
+
+
+def parse_ticket_info(url):
+    """Парсит информацию о билетах с указанного     URL и возвращает данные в виде словаря.
+    """
+
+    TICKETS_QUERY = """
+    {
+    train_trips[] {
+        train_name(The name or number of the train)
+
+        departure_date(The departure date, for example 7 September or 07.09)
+        departure_time(The time of departure)
+
+        arrival_date(The arrival date, for example 8 September or 08.09)
+        arrival_time(The time of arrival)
+
+        travel_duration(The total time the trip takes)
+
+        ticket_categories[] {
+        ticket_type(The class or type of ticket, for example platzkart, coupe, lux)
+        available_seats(The number of free places)
+        price(The cost of the ticket)
+        }
+    }
+    }
+    """
+    answer = []
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = agentql.wrap(browser.new_page())
+
+        for current_link in url:
+            page.goto(current_link)
+            page.wait_for_timeout(5000)
+            try:
+                result = page.query_data(TICKETS_QUERY)
+                print(json.dumps(result, ensure_ascii=False, indent=4))
+                answer.append([result,current_link]) 
+            except PlaywrightTimeoutError:
+                print(f"Не удалось получить данные для {current_link}. Пропускаю эту ссылку.")
+                continue
+        browser.close()       
+    
+    return answer
+
+llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.5,api_key=openai_api_key)  # type: ignore
+system_prompt = """
+Ты помощник для поиска билетов.
+Твоя задача:
+- анализировать найденные билеты
+- выбирать самый дешевый вариант по каждому классу (платцкарт, купе, люкс) и рядом вывести ссылку на страницу с этим билетом
+- выбирать самый оптимальный вариант по цене и времени
+- отвечать кратко на русском
+"""
+
+
+links = get_links(url, date_from, date_to)
+tickets = parse_ticket_info(links)
+
+response = llm.invoke([
+    {"role": "system", "content": system_prompt},
+    {"role": "user", "content": f"""
+    Вот найденные билеты:
+
+    {tickets}
+
+    Выбери:
+    1. Самый дешевый билет по каждому классу
+    2. Самый оптимальный вариант по цене и времени
+    3. Дай ссылку рядом с вариантом
+    """}
+])
+
+
+
+print(response.content)
